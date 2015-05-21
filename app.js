afcc614628768742bb1c2f635a44eedee8462be9
arichter <arichter@student.ethz.ch>
2015-05-21 17:10:55 +0200

    compare arrays with stringify

--- app.js
+++ app.js
@@ -144,7 +144,7 @@ setInterval(function(){
 	collection.find().toArray(function(err, docs) {
 		if (docs.length > 0) {
 			console.log(docs);
-			if (monitoringKeywords.equals(docs) === false) {
+			if (JSON.stringify(monitoringKeywords) != JSON.stringify(docs)) {
 		    	monitoringKeywords = docs;
 		    	establishTwitterConnection();
 		    	console.log("New keywords:");
@@ -167,32 +167,3 @@ setInterval(function(){
 
 app.listen(port);
 console.log("Server listening on port " + port);
-
-
-
-
-
-
-Array.prototype.equals = function (array) {
-    // if the other array is a falsy value, return
-    if (!array)
-        return false;
-
-    // compare lengths - can save a lot of time 
-    if (this.length != array.length)
-        return false;
-
-    for (var i = 0, l=this.length; i < l; i++) {
-        // Check if we have nested arrays
-        if (this[i] instanceof Array && array[i] instanceof Array) {
-            // recurse into the nested arrays
-            if (!this[i].equals(array[i]))
-                return false;       
-        }           
-        else if (this[i] != array[i]) { 
-            // Warning - two different object instances will never be equal: {x:20} != {x:20}
-            return false;   
-        }           
-    }       
-    return true;
-} 
