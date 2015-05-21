2950e725b39a71f13a9af0e8e3b923c2acdb0004
arichter <arichter@student.ethz.ch>
2015-05-21 17:04:06 +0200

    compare arrays the right way

--- app.js
+++ app.js
@@ -143,11 +143,14 @@ setInterval(function(){
 	var collection = myDb.collection(dbKeywordsCollection);
 	collection.find().toArray(function(err, docs) {
 		if (docs.length > 0) {
-			if (monitoringKeywords != docs) {
+			console.log(docs);
+			if (monitoringKeywords.equals(docs) === false) {
 		    	monitoringKeywords = docs;
 		    	establishTwitterConnection();
 		    	console.log("New keywords:");
 		    	console.log(monitoringKeywords);
+		    } else {
+		    	console.log("No new keywords");
 		    }
 	    } else {
 	    	console.log("No keywords in database!");
@@ -165,3 +168,31 @@ setInterval(function(){
 app.listen(port);
 console.log("Server listening on port " + port);
 
+
+
+
+
+
+Array.prototype.equals = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+
+    // compare lengths - can save a lot of time 
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].equals(array[i]))
+                return false;       
+        }           
+        else if (this[i] != array[i]) { 
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;   
+        }           
+    }       
+    return true;
+} 
